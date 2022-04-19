1d93ff0540d8be015f1afd36d1ce86b190ae1256
Lucas Leandro <lucasjleandro@gmail.com>
2022-04-19 00:19:10 -0300

    Primeiro Commit

---
+++ challenge-backend-3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace challenge_backend_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
